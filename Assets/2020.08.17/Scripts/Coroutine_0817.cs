266a5af0b221a6a164a5c4af6e8cacf2c8d4e3b4
HysenChan <HysenChan@163.com>
2020-08-17 16:15:57 +0800

    【添加】使用协程去更新时间，每隔1s生成100个Cube

---
+++ Assets/2020.08.17/Scripts/Coroutine_0817.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutine_0817 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CreateCube());
+    }
+
+    IEnumerator CreateCube()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position=Vector3.one*i;
+        }
+        yield return new WaitForSeconds(1f);
+    }
+}
