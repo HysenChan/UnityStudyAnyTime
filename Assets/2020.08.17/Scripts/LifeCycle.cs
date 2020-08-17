70f56e82d2b1da2704d592d194568d5c7e05e23c
HysenChan <HysenChan@163.com>
2020-08-17 15:24:15 +0800

    【添加】添加生命周期的先后顺序调试

---
+++ Assets/2020.08.17/Scripts/LifeCycle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCycle : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("OnApplicationQuit");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UnityStudyAnyTime
 The catalog is classified based on time.
+
+## 2020.08.17
+- Unity的生命周期
